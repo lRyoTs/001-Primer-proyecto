968eaa32f4cf79abcada73cab2c88dc373c1091c
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2022-11-18 08:02:36 +0100

    updated movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+    private float verticalInput;
+    private float horizontalInput;
+    private int currentSpeed;
+    private const int speedForward = 5;
+    private const int speedBackward = 3;
+    private int turnSpeed = 60;
+    private Vector3 initialPos = new Vector3(8,0,-14);
     // Start is called before the first frame update
     void Start()
     {
-        
+        initiateChar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Forward o backward movement
+        if (verticalInput < 0)
         {
-            transform.localPosition += Vector3.forward;
+            currentSpeed = speedBackward; //get backward speed
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        else {
+            currentSpeed = speedForward; //gt forward speed
+        }
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime * verticalInput);
+        
+        
+        //Lateral movement
+        if (Input.GetKey(KeyCode.D)||(Input.GetKey(KeyCode.A)))
         {
-            transform.localPosition += Vector3.back;
+             transform.Translate(Vector3.right * currentSpeed * Time.deltaTime * horizontalInput);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        //Rotattion movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.localPosition += Vector3.right;
+           transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
         }
-        if (Input.GetKeyDown(KeyCode.A))
+
+        //Reset position
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            transform.localPosition += Vector3.left;
+            initiateChar();
         }
+
     }
+
+    //Initiate character
+    private void initiateChar()
+    {
+        transform.SetPositionAndRotation(initialPos, Quaternion.Euler(0, -90, 0));
+    }
+
+
 }
